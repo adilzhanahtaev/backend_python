57267b339e83193e776c370ebd5c0b83fbbd40ea
Adilka <adilzhanahtaev20@icloud.com>
2026-03-23 01:22:13 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from flask import render_template as ren_templ # with as we can reduce 
+
+app = Flask (__name__)
+
+#functions for navigating through site pages    
+@app.route ('/') #in (/name of site page) 
+def home():
+    return ren_templ('index.html') # here in () we indicate the path to the file of the desired page {with }
+
+if __name__ == '__main__':
+    app.run(debug=True)
